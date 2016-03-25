b8cceccb941f1f160e9cf3793273cb5250dedea6
Samisdat <bastian@pertz.eu>
2016-03-25 22:06:13 +0000

    add navigation

--- themes/conway/source/js/app.js
+++ themes/conway/source/js/app.js
@@ -8,13 +8,28 @@
 		var $conway;
 		var invisible = false;
 
+		var $navbar;
+		var fixed = false;
+
 		var scroll = function(){
-			if(invisible === false && $(window).scrollTop() > 250){
+			
+			var scrollTop = $(window).scrollTop();
+
+			if(fixed === false && scrollTop > 200){
+				fixed = true;
+				$navbar.addClass('navbar-fixed-top');
+			}
+			else if(fixed === true && scrollTop <= 200){
+				fixed = false;
+				$navbar.removeClass('navbar-fixed-top');
+			}
+
+			if(invisible === false && scrollTop > 250){
 				invisible = true;
 				$conway.trigger('invisible', {invisible:invisible});
 				$conway.addClass('invisible');
 			}
-			else if(invisible === true && $(window).scrollTop() <= 250){
+			else if(invisible === true && scrollTop <= 250){
 				invisible = false;
 				$conway.trigger('invisible', {invisible:invisible});
 				$conway.removeClass('invisible');
@@ -22,6 +37,7 @@
 		};
 		var ready = function() {
 			$conway = $('#conway');
+			$navbar = $('.navbar');
 			$(window).scroll(scroll);
 			scroll();
 		};
