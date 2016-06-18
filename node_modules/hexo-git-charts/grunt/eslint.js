a3b88811828a6164dfee8399343104a25c7579d8
samisdat <bastian.sackermann@gmail.com>
2016-06-18 22:18:37 +0000

    add charts submodule

---
+++ node_modules/hexo-git-charts/grunt/eslint.js
@@ -0,0 +1,25 @@
+/*global module */
+
+'use strict';
+
+var files = [
+    'grunt/*.js',
+    'lib/**/*.js',
+    'test/**/*.js'
+];
+
+var eslint = {
+    lint: {
+        src: files
+    },
+    lintAndFix: {
+        options: {
+            fix: true
+        },
+        src: files
+    }
+};
+
+
+
+module.exports = eslint;
