aa0b15eabcd5ef4a9714c36f96a99a9a4dd868e3
samisdat <bastian.sackermann@gmail.com>
2016-06-22 22:40:12 +0000

    d3 chart with label next to last point

--- themes/conway/source/js/github-charts/github-charts.js
+++ themes/conway/source/js/github-charts/github-charts.js
@@ -6,136 +6,94 @@
 
     Samisdat.GithubChart = ( function() {
         
-        var $gitChart;
-        var gitChart;
+        var svg;
 
-        var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var setupD3 = function(){
+            var margin = {top: 30, right: 50, bottom: 30, left: 50};
+            var width = 600 - margin.left - margin.right;
+            var height = 270 - margin.top - margin.bottom;
 
-        var randomScalingFactor = function() {
-            return Math.round(Math.random() * 25);
+            var parseDate = d3.time.format("%d-%b-%y").parse;
+
+            var x = d3.time.scale().range([0, width]);
+            var y = d3.scale.linear().range([height, 0]);
+
+            var xAxis = d3.svg.axis().scale(x)
+                .orient("bottom").ticks(5);
+
+            var yAxis = d3.svg.axis().scale(y)
+                .orient("left").ticks(5);
+
+            var valueline = d3.svg.line()
+                .x(function(d) { return x(d.date); })
+                .y(function(d) { return y(d.close); });
+                
+            var valueline2 = d3.svg.line()
+                .x(function(d) { return x(d.date); })
+                .y(function(d) { return y(d.open); });
+      
+            svg = d3.select("#foo")
+                .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+            // Get the data
+            d3.csv("/images/data2b.csv", function(error, data) {
+                console.log(data)
+                data.forEach(function(d) {
+                    d.date = parseDate(d.date);
+                    d.close = +d.close;
+                    d.open = +d.open;
+                });
+                
+                // Scale the range of the data
+                x.domain(d3.extent(data, function(d) { return d.date; }));
+                y.domain([0, d3.max(data, function(d) { return Math.max(d.close, d.open); })]);
+
+                svg.append("path")      // Add the valueline path.
+                    .attr("class", "line")
+                    .attr("d", valueline(data));
+
+                svg.append("path")      // Add the valueline2 path.
+                    .attr("class", "line")
+                    .style("stroke", "red")
+                    .attr("d", valueline2(data));
+
+                svg.append("g")         // Add the X Axis
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+                svg.append("g")         // Add the Y Axis
+                    .attr("class", "y axis")
+                    .call(yAxis);
+
+                svg.append("text")
+                    .attr("transform", "translate(" + (width+3) + "," + y(data[0].open) + ")")
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", "start")
+                    .style("fill", "red")
+                    .text("Open");
+
+                svg.append("text")
+                    .attr("transform", "translate(" + (width+3) + "," + y(data[0].close) + ")")
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", "start")
+                    .style("fill", "steelblue")
+                    .text("Close");
+
+            });
+
         };
 
-        var data = (function(){
 
-            var repos = [
-                '/FreeCodeCamp/FreeCodeCamp',
-                '/fed135/Kalm',
-                '/gittips\/tips',
-                '/bahmutov/javascript-journey',
-                '/UDST/vizicities',
-                '/benjamn/reify',
-                '/angular/angular.js',
-                '/getify/You-Dont-Know-JS',
-                '/parkjs814/AlgorithmVisualizer',
-                '/facebook/react',
-                '/GoogleChrome/lighthouse',
-                '/nickberens360/atomic-docs',
-                '/airbnb/javascript',
-                '/weixin/tmt-workflow',
-                '/luruke/barba.js',
-                '/SamyPesse/gitkit-js',
-                '/vuejs/vue',
-                '/yoshuawuyts/choo',
-                '/nodejs/node',
-                '/reactjs/redux',
-                '/nylas/N1',
-                '/tj/react-enroute',
-                '/dvLden/Vidage',
-                '/weixin/WeFlow',
-                '/algolia/places'
-            ];
 
-            var dataSetConfig = {
-                fill: false,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 5,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10                                    
-            };
-
-            var datasets = [];
-
-            repos.forEach(function(repo, index){
-
-                var position = index + 1;
-
-                var dataset = {
-                    label: repo,
-                    data: [position, position, position, position, position, position, position]
-                };
-
-                dataset = $.extend({}, dataset, dataSetConfig);
-                datasets.push(dataset);
-            })
-
-            return datasets;
-
-        })();
-
-        var config = {
-            type: 'line',
-            data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: data
-            },
-            options: {
-                responsive: true,
-                legend: {
-                    position: 'right',
-                },
-                hover: {
-                    mode: 'label'
-                },
-                scales: {
-                    xAxes: [{
-                        display: false,
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Day'
-                        }
-                    }],
-                    yAxes: [{
-                        display: false,
-                        scaleLabel: {
-                            display: false,
-                            labelString: 'Position'
-                        },
-                        ticks: {
-                            suggestedMin: -1,
-                            suggestedMax: 26,
-                        }
-
-                    }]
-                },
-                title: {
-                    display: false,
-                    text: 'Chart.js Line Chart - Legend'
-                }
-            }
-        };
 
         var ready = function() {
-            $gitChart = $('.gitChart canvas');
-
-            if(undefined === $gitChart.get(0)){
-                return;
-            }
-
-            var ctx = $gitChart.get(0).getContext('2d');
-
-            gitChart = new Chart(ctx, config);
-
+            setupD3();
         };
 
         $(document).ready(ready);
