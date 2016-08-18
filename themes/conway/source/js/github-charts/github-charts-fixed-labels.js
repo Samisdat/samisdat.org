2680146b78f2845f8d8af537d97910a6e8b1cb18
Samisdat <bastian@pertz.eu>
2016-08-18 21:18:30 +0000

    remove moving lineaxis

--- themes/conway/source/js/github-charts/github-charts-fixed-labels.js
+++ themes/conway/source/js/github-charts/github-charts-fixed-labels.js
@@ -2,7 +2,6 @@
 
 (function($) {
 
-
     $.fn.disableSelection = function() {
         return this
             .attr('unselectable', 'on')
@@ -19,11 +18,12 @@
         var inlineAxis;
 
         var margin = {
-            top: 40, 
-            right: 3, 
+            top: 20, 
+            right: 6, 
             bottom: 10, 
-            left: 3
+            left: 10
         };
+        
         var radius = 5;
         var width;
         var height;
@@ -49,78 +49,6 @@
             return id; 
         };
 
-        var roundDate = function(dateToRound){
-
-            var before = new Date(dateToRound.getTime());
-            before.setHours(0);
-            before.setMinutes(0, 0, 0);
-
-            var after = new Date(dateToRound.getTime());
-
-            after.setDate(after.getDate() + 1);
-
-            after.setHours(0);
-            after.setMinutes(0, 0, 0);
-
-            return {
-                before: before,
-                after: after                
-            };
-        };
-
-        var createInlineAxis = function(){
-            inlineAxis = svg.append('g')
-                .attr('class', 'inlineAxis')
-            ;
-
-            inlineAxis
-                .append('line')
-                .attr('x1', 0)
-                .attr('x2', 0)
-                .attr('y1', -1 * margin.top)
-                .attr('y2', height)
-            ;
-
-            series.forEach(function(serie){
-
-                var firstOfSerie = serie[0];
-
-                inlineAxis.append('circle')
-                    .attr('id', 'point-' + keyToId(firstOfSerie.key))
-                    .attr('r', radius)
-                    .style('fill', z(firstOfSerie.key))
-                    .style('stroke', 'none')
-                    .attr('cx', 0)
-                    .attr('cy', 0)
-                ;
-                /*
-                inlineAxis.append('text')
-                    .attr('id', 'label-' + firstOfSerie.key.replace('/', '-'))
-                    .style('fill', 'black')
-                    .style('stroke', 'black')
-                    .text(firstOfSerie.key)
-                    .attr('text-anchor', 'end')
-                    .style('opacity', 0)
-                    .attr('x', -20)
-                    .attr('transform', 'translate(0,' + y(firstOfSerie.value) + ')')
-                ;
-                */
-            });
-
-            var labelWidth = 0;
-
-            inlineAxis.selectAll('text').each(function() {
-                if (this.getBBox().width > labelWidth){
-                    labelWidth = this.getBBox().width;
-                }
-
-                d3.select(this).attr('width', this.getBBox().width);
-
-            });
-
-            labelSwitchThreshold = labelWidth + margin.left;
-        };
-
         var getFirstValueOfSerie = function(serie){
 
             var firstNotNullValue;
@@ -139,285 +67,6 @@
 
             return serie[firstNotNullValue];
         };
-
-        var lastDate;
-
-        var __moveInlineAxis = function(xPos){
-            if(margin.left > xPos){
-                xPos = margin.left;
-            }
-
-            if(margin.left + width < xPos){
-                xPos = margin.left + width;
-            }
-
-            var xValue = xPos - margin.left;
-
-            var dateOnPos = x.invert(xPos);
-
-            var dateRounded = roundDate(dateOnPos);
-
-            if(lastDate === dateRounded){
-                return;
-            }
-
-            lastDate = dateRounded;
-
-            console.log(lastDate, dateRounded);
-            inlineAxis
-                .attr('transform', 'translate(' + x(dateOnPos) + ',' + margin.top + ')')
-            ;
-
-            var checkValueOnDate = function(date, serie){
-
-                var hasValueOnDate = false;
-
-                serie.forEach(function(day){
-                    if(date.toString() !== day.date.toString()){
-                        return true;
-                    }
-
-                    if(null === day.value){
-                        return true;
-                    }
-
-                    hasValueOnDate = true;
-                    return false;
-                    
-                });
-
-                return hasValueOnDate;
-            };
-
-            var hideInlineLabel = function(item){
-                inlineAxis.select('#point-' + item.key.replace('/', '-'))
-                    .style('opacity', 0)
-                ;
-
-
-                var label = inlineAxis.select('#label-' + item.key.replace('/', '-'))
-                    .style('opacity', 0)
-                ;
-
-            };
-
-            svg.selectAll('.serie path').each(function(serie){
-
-                var hasValueOnDate = checkValueOnDate(dateRounded, serie);
-
-                var item = serie[bisectDate(serie, dateRounded)];
-                console.log(hasValueOnDate, item)
-                if(null === item.value){
-                    hideInlineLabel(item);
-                    return;
-                }
-
-                var pathEl = this;
-                var pathLength = pathEl.getTotalLength();
-
-                var offsetLeft = 0;
-
-                var x = xValue; 
-
-                var beginning = x, end = pathLength, target;
-
-                var pos;
-
-                while (true) {
-                    target = Math.floor((beginning + end) / 2);
-                    pos = pathEl.getPointAtLength(target);
-                    
-                    if ((target === end || target === beginning) && pos.x !== x) {
-                        break;
-                    }
-
-                    if (pos.x > x){
-                        end = target;
-                    }      
-                    else if (pos.x < x){ 
-                        beginning = target;
-                    }
-                    else{
-                        break; //position found
-                    }
-                }
-
-                var point = pos.y;
-
-                var firstOfSerie = getFirstValueOfSerie(serie);
-
-                inlineAxis.select('#point-' + firstOfSerie.key.replace('/', '-'))
-                    .attr('transform', 'translate(' + 0 + ',' + point + ')')
-                    .style('opacity', 1)
-                ;
-
-                var transformX = -10;
-
-                var label = inlineAxis.select('#label-' + firstOfSerie.key.replace('/', '-'))
-                    .style('opacity', 1)
-                ;
-
-                console.log(label)
-
-                if (xPos < labelSwitchThreshold){
-                    var labelWidth = parseInt(label.attr('width'), 10);
-                    transformX = labelWidth + 10;
-                }
-
-                if(lastY === y(item.value)){
-                    label
-                        .attr('transform', 'translate(0,' + y(item.value) + ')')
-                        .attr('x',  transformX)
-                    ;
-                }
-                else{
-                    label
-                        .attr('x', transformX)                    
-                        .attr('transform', 'translate(0,' + y(item.value) + ')')
-                    ;
-                }
-
-
-                return;
-
-                var lastY = label.attr('data-last-y')   ;
-                if(null !== lastY){
-                    lastY = lastY * 1;
-                }
-                else{
-                    lastY = y(item.value);   
-                }
-
-                label.attr('data-last-y', y(item.value));
-
-                if (xPos < labelSwitchThreshold){
-                    var labelWidth = parseInt(label.attr('width'), 10);
-                    transformX = labelWidth + 10;
-                }
-
-                if(lastY === y(item.value)){
-                    label
-                        .attr('transform', 'translate(0,' + y(item.value) + ')')
-                        .attr('x',  transformX)
-                    ;
-                }
-                else{
-                    label
-                        .attr('x', transformX)                    
-                        .attr('transform', 'translate(0,' + lastY + ')')
-                        .transition()
-                        .attr('transform', 'translate(0,' + y(item.value) + ')')
-                    ;
-                }
-
-            });
-
-        };
-
-        var checkValueOnDate = function(date, serie){
-
-            var hasValueOnDate = false;
-
-            serie.forEach(function(day){
-                if(date.toString() !== day.date.toString()){
-                    return true;
-                }
-
-                if(null === day.value){
-                    return true;
-                }
-
-                hasValueOnDate = true;
-                return false;
-                
-            });
-
-            return hasValueOnDate;
-        };
-
-        var moveInlineAxis = function(xPos){
-
-            if(margin.left > xPos){
-                xPos = margin.left;
-            }
-
-            if(margin.left + width < xPos){
-                xPos = margin.left + width;
-            }
-
-            var xValue = xPos - margin.left;
-
-            var dateOnPos = x.invert(xPos);
-
-            inlineAxis
-                .attr('transform', 'translate(' + x(dateOnPos) + ',' + margin.top + ')')
-            ;
-
-            var datesArounded = roundDate(dateOnPos);
-
-            svg.selectAll('.serie path').each(function(serie){
-
-                var hasValueBefore = checkValueOnDate(datesArounded.before, serie);
-                var hasValueAfter = checkValueOnDate(datesArounded.after, serie);
-
-                var firstOfSerie = getFirstValueOfSerie(serie);
-
-                var movingPoint = inlineAxis.select('#point-' + keyToId(firstOfSerie.key));
-
-
-                if(false === hasValueBefore || false === hasValueAfter){
-                    movingPoint.style('opacity', 0);
-                    return;
-                }
-
-                movingPoint.style('opacity', 1);
-
-                var item = serie[bisectDate(serie, datesArounded.before)];
-
-
-                var pathEl = this;
-                var pathLength = pathEl.getTotalLength();
-
-                var offsetLeft = 0;
-
-                var x = xValue; 
-
-                var beginning = x, end = pathLength, target;
-
-                var pos;
-
-                while (true) {
-                    target = Math.floor((beginning + end) / 2);
-                    pos = pathEl.getPointAtLength(target);
-                    
-                    if ((target === end || target === beginning) && pos.x !== x) {
-                        break;
-                    }
-
-                    if (pos.x > x){
-                        end = target;
-                    }      
-                    else if (pos.x < x){ 
-                        beginning = target;
-                    }
-                    else{
-                        break; //position found
-                    }
-                }
-
-                var point = pos.y;
-
-                movingPoint.attr('transform', 'translate(' + 0 + ',' + point + ')')
-
-                console.log(datesArounded.before, datesArounded.before);
-                console.log(serie[0].key, hasValueBefore, hasValueAfter);
-            });
-
-
-            console.log(dateOnPos, datesArounded);
-
-
-        };        
 
         var createBackgroundAxis = function(){
             var formatDayMonth = d3.timeFormat('%d.%m');
@@ -496,8 +145,6 @@
 
             createBackgroundAxis();
 
-            createInlineAxis();
-
             var g = svg.append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
             ;
@@ -516,8 +163,8 @@
 
             serie.append('path')
                 .attr('class', 'line')
-                .style('stroke', function(d) { return z(d[0].key); })
-                .attr("d", line)
+                .style('stroke', function(d) { return z(d[0].index); })
+                .attr('d', line)
                 /*
                 .attr('d', d3.line()//.curve(d3.curveMonotoneX)
                 .x(function(d) { return x(d.date); })
@@ -526,40 +173,22 @@
             ;
 
             series.forEach(function(serie){
+                console.log(serie[0].value)
                 serie.forEach(function(value){
                     if(null === value.value){
                         return true;
                     }
 
                     svg.append("circle")
-                        .attr("r", 4)
-                        .style('stroke', function(d) { return z(value.key); })
-                        .style('fill', function(d) { return z(value.key); })
+                        .attr("r", 5)
+                        .style('stroke', function(d) { return z(value.index); })
+                        .style('fill', function(d) { return z(value.index); })
                         .attr("cx", x(value.date) + margin.left )
                         .attr("cy", y(value.value) + margin.top );
                 });
             });
 
         };
-
-        var addEventListener = function(){
-
-            svg.on('mousemove', function() {
-
-                var mouseX = d3.mouse(this)[0];
-                moveInlineAxis(mouseX);
-
-            });
-
-            svg.on('touchmove', function() {
-
-                var mouseX = d3.mouse(this)[0];
-                moveInlineAxis(mouseX);
-
-            });
-
-        };
-
 
         var getSeriesFromDom = function(){
             dates = $chartList.data('dates');
@@ -619,9 +248,10 @@
 
                 var serie = [];
 
-                positions.forEach(function(position, index){
-                    var date = dates[index];
+                positions.forEach(function(position, index2){
+                    var date = dates[index2];
                     serie.push({
+                        index: index,
                         date: date,
                         key: $.trim(repoName),
                         value: position
@@ -652,17 +282,13 @@
 
             $row.append($svg);
 
-            //$chartList.remove();
+            $chartList.remove();
 
         };
 
         getSeriesFromDom();
         markup();
         createChart();
-
-        addEventListener();
-
-        moveInlineAxis(width + margin.left - 1);
 
     };
 
