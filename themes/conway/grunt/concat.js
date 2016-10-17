696eba87190c889a5f3cce84c2d20e1e58f956de
samisdat <bastian.sackermann@gmail.com>
2016-10-17 21:15:24 +0000

    concat correct files

--- themes/conway/grunt/concat.js
+++ themes/conway/grunt/concat.js
@@ -5,6 +5,8 @@ var concat = {
     dist: {
         src: [
             'source/bower_components/jquery/jquery.min.js',
+            'source/bower_components/tether/javascripts/tether.min.js',
+            'source/bower_components/bootstrap/javascripts/bootstrap.min.js',
             'source/bower_components/highlightjs/highlight.pack.min.js',
             'source/js/lib/rAF.js',
             'source/js/conway/patterns.js',
@@ -13,7 +15,8 @@ var concat = {
             'source/js/conway/generation.js',
             'source/js/conway/observation.js',
             'source/js/conway/canvas.js',
-            'source/js/ghost/app.js'
+            'source/js/github-charts/github-charts-fixed-labels.js',
+            'source/js/app.js',
         ],
         dest: 'source/js/conway.js',
     }
