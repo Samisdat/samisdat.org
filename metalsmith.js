dea76dfbe520f90b0780dfea84f870ada709fc38
samisdat <bastian@pertz.eu>
2019-01-22 22:17:38 +0000

    render scss by metalsmith

--- metalsmith.js
+++ metalsmith.js
@@ -1,10 +1,15 @@
 var Metalsmith = require('metalsmith');
+var sass = require('metalsmith-sass');
 var redirect = require('metalsmith-redirect');
 metalsmith.use(redirect({
   '/hexo-staging.html': '/blog/hexo-staging/',
   '/bar.html': '/img/'
 }));
 
+metalsmith.use(sass({
+  outputStyle: 'expanded',
+  outputDir: 'css/'
+}));
 metalsmith.build(function (err, files) {
   if (err) { throw err; }
 });
