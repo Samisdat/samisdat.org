4260858f743b17a96dd9b9ab46057e39f7ef6b6c
Samisdat <bastian@pertz.eu>
2024-12-30 00:34:56 +0100

    npm run format

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import { FC } from "react";
+import { FC } from 'react';
 
 export const Car: FC<{ x: number; width: number }> = ({ x, width }) => {
-  return (
-    <>
-      <rect
-        id="Wagon"
-        x={x}
-        y="275.156"
-        width={width}
-        height="11"
-        strokeWidth="3"
-        style={{ fill: "#e91600", stroke: "#381E24" }}
-      />
-    </>
-  );
+    return (
+        <>
+            <rect
+                id="Wagon"
+                x={x}
+                y="275.156"
+                width={width}
+                height="11"
+                strokeWidth="3"
+                style={{ fill: '#e91600', stroke: '#381E24' }}
+            />
+        </>
+    );
 };
--- src/components/Heaven/Sun.tsx
+++ src/components/Heaven/Sun.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import {FC, useEffect, useState} from "react";
+import { FC, useEffect, useState } from 'react';
 
 type SunPosition = {
     x: number;
     y: number;
-}
+};
 
 const width = 1100;
 const height = 350;
-const center:SunPosition =
-    {x:750, y:220}
-
+const center: SunPosition = { x: 750, y: 220 };
 
 export const Sun: FC<{}> = () => {
     const [pos, setPos] = useState<SunPosition>(center);
@@ -19,33 +17,31 @@ export const Sun: FC<{}> = () => {
     const [angle, setAngle] = useState<number>(0);
 
     useEffect(() => {
-
         setTimeout(() => {
             setAngle(angle + 0.01);
         }, 60);
     }, [angle]); // Make sure the effect runs only once
 
     useEffect(() => {
+        console.log(angle, Math.sin(angle));
 
-console.log(angle, Math.sin(angle), )
-
-        const x = Math.cos(angle) * width / 2 + center.x
-        const y = Math.sin(angle) * height / 2 + center.y
+        const x = (Math.cos(angle) * width) / 2 + center.x;
+        const y = (Math.sin(angle) * height) / 2 + center.y;
 
         setPos({
-            x,y
-        })
-
+            x,
+            y,
+        });
     }, [angle]); // Make sure the effect runs only once
 
-
-
     return (
-        <circle cx={pos.x} cy={pos.y} r="30"
-                style={{
-                    fill: "#F0FF5E",
-                }}/>
-
+        <circle
+            cx={pos.x}
+            cy={pos.y}
+            r="30"
+            style={{
+                fill: '#F0FF5E',
+            }}
+        />
     );
 };
-
