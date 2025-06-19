a66250e77cd2f95f3e6fef4b8353acce855cc383
Samisdat <bastian@pertz.eu>
2025-06-19 21:29:33 +0200

    animated cars

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,41 +1,40 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { Car } from './Car';
-
 export const Cars = () => {
-    const [x1, setX1] = useState(250);
-    const [x2, setX2] = useState(1200);
-
-    const width = 81.778;
-
-    useEffect(() => {
-        setTimeout(() => {
-            let nextX1 = x1 + 1;
-            let nextX2 = x2 - 1;
-
-            if (nextX1 > 1500) {
-                nextX1 = -width;
-            }
-            if (nextX2 < -width) {
-                nextX2 = 1500;
-            }
-
-            setX1(nextX1);
-            setX2(nextX2);
-        }, 10);
-    }, [x1, x2]); // Make sure the effect runs only once
+    const duration = '80s';
+    const start = 100;
+    const stop = 1400;
 
     return (
         <>
-            <Car
-                x={x1}
-                width={width}
-            />
-            <Car
-                x={x2}
-                width={width}
-            />
+            <rect
+                x={start}
+                y="274"
+                width={'82'}
+                height="12"
+                strokeWidth="3"
+                style={{ fill: '#00AFEB', stroke: '#000051' }}
+            >
+                <animate
+                    attributeName="x"
+                    values={`${start};${stop};${start}`}
+                    dur={duration}
+                    repeatCount="indefinite"
+                />
+            </rect>
+            <rect
+                x={1330}
+                y="274"
+                width={'82'}
+                height="12"
+                strokeWidth="3"
+                style={{ fill: '#00AFEB', stroke: '#000051' }}
+            >
+                <animate
+                    attributeName="x"
+                    values={`${stop};${start};${stop}`}
+                    dur={duration}
+                    repeatCount="indefinite"
+                />
+            </rect>
         </>
     );
 };
