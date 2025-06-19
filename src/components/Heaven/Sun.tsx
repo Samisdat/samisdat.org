a19a1f9947af8353e36a1c7e3d011d80f57c78c7
Samisdat <bastian@pertz.eu>
2025-06-19 21:29:18 +0200

    animate sun with just svg

--- src/components/Heaven/Sun.tsx
+++ src/components/Heaven/Sun.tsx
@@ -1,47 +1,23 @@
 'use client';
 
-import { FC, useEffect, useState } from 'react';
+import { dayLength } from '@/components/const';
+import { FC } from 'react';
 
-type SunPosition = {
-    x: number;
-    y: number;
-};
-
-const width = 1100;
-const height = 350;
-const center: SunPosition = { x: 750, y: 220 };
-
-export const Sun: FC<{}> = () => {
-    const [pos, setPos] = useState<SunPosition>(center);
-
-    const [angle, setAngle] = useState<number>(0);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setAngle(angle + 0.01);
-        }, 60);
-    }, [angle]); // Make sure the effect runs only once
-
-    useEffect(() => {
-        console.log(angle, Math.sin(angle));
-
-        const x = (Math.cos(angle) * width) / 2 + center.x;
-        const y = (Math.sin(angle) * height) / 2 + center.y;
-
-        setPos({
-            x,
-            y,
-        });
-    }, [angle]); // Make sure the effect runs only once
-
+export const Sun: FC = () => {
     return (
-        <circle
-            cx={pos.x}
-            cy={pos.y}
-            r="30"
-            style={{
-                fill: '#F0FF5E',
-            }}
-        />
+        <>
+            <circle
+                r="30"
+                style={{
+                    fill: '#F0FF5E',
+                }}
+            >
+                <animateMotion
+                    dur={`${dayLength}s`}
+                    repeatCount="indefinite"
+                    path="M149.988,297.533c-1.021,-255.076 600.006,-256.533 600.006,-256.533c0,-0 596.385,-0.813 600.006,256.533"
+                />
+            </circle>
+        </>
     );
 };
