e021af39499576bc69f34153d08ed6286f77df89
Samisdat <bastian@pertz.eu>
2025-10-23 21:02:39 +0200

    configure project as publishable npm package
    - Rename to @samisdat/wtal-panorama
    - Add package exports (CJS, ESM, types)
    - Configure tsup for library builds
    - Add src/index.tsx as package entry point
    - Create tsconfig.lib.json for library compilation
    - Add dist/ to .gitignore
    - Update peerDependencies to React 19
    - Bundle SunCalc into package (no external dependencies)

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { Panorama } from './components/Panorama';
+import { TalProvider } from './lib/TalContext';
+
+export const WtalPanorama = () => (
+    <TalProvider>
+        <Panorama />
+    </TalProvider>
+);
+
+export { Panorama } from './components/Panorama';
+export { TalProvider } from './lib/TalContext';
