b095f9ffb2e6c16480aaf380e3aefd25a0cb5210
Samisdat <bastian@pertz.eu>
2025-12-14 00:01:58 +0100

    update imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-'use client';
-import Panorama from './components/Panorama';
-import { TalProvider } from './lib/TalContext';
+"use client";
+import { Panorama } from "./components/Panorama";
+import { TalProvider } from "./lib/TalContext";
 
 export const WtalPanorama = () => (
-    <TalProvider>
-        <Panorama />
-    </TalProvider>
+  <TalProvider>
+    <Panorama />
+  </TalProvider>
 );
-
-export { default as Panorama } from './components/Panorama';
-export { TalProvider } from './lib/TalContext';
