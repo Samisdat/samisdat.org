b63c8ab519a8660787dad0033d04a9da287e3a85
Samisdat <bastian@pertz.eu>
2025-12-16 22:22:08 +0100

    reduce tech stack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import withLinaria, { LinariaConfig } from 'next-with-linaria';
-
+import withLinaria, { LinariaConfig } from "next-with-linaria";
 const nextConfig: LinariaConfig = {
   linaria: {
     fastCheck: true,
   },
+  transpilePackages: ["next-mdx-remote"],
 };
 
 export default withLinaria(nextConfig);
+
