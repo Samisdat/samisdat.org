b6728dd1efdfc1d807acce26adfb93930f625e62
Samisdat <bastian@pertz.eu>
2026-01-19 23:54:59 +0100

    lint and format

--- codesandboxes/foo/example-2/src/index.tsx
+++ codesandboxes/foo/example-2/src/index.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 
-import { App } from "./App";
+import { App } from './App';
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!);
 root.render(
     <StrictMode>
         <App />
