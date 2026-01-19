106defadfc46119d0cab8e6cde10cda404fc9ce1
Samisdat <bastian@pertz.eu>
2026-01-20 00:19:57 +0100

    use css props to pass config into css

--- codesandboxes/animations/animation-compare/src/Css.tsx
+++ codesandboxes/animations/animation-compare/src/Css.tsx
@@ -1,8 +1,18 @@
+import { CSSProperties } from 'react';
 import { Checkerboard } from './Checkerboard';
 import { config, DemoAnimationsCompareProps } from './shared';
 
 export const Css = ({ isPlaying, resetTrigger }: DemoAnimationsCompareProps) => (
-    <svg viewBox="0 0 200 30">
+    <svg
+        viewBox="0 0 200 30"
+        style={
+            {
+                '--distance': `${config.distance}px`,
+                '--duration': `${config.duration}ms`,
+                '--repeats': config.repeats,
+            } as CSSProperties
+        }
+    >
         <Checkerboard
             width={230}
             height={30}
