baa9da1ddd25a96b940a7fe088e98ca332dbfc4b
Samisdat <bastian@pertz.eu>
2026-05-18 23:42:46 +0200

    add a story for DemoBox

---
+++ packages/storybook/src/DemoBox.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { DemoBox } from "@samisdat/ui-components/DemoBox";
+
+const meta = {
+  title: "DemoBox",
+  component: DemoBox,
+  tags: ["autodocs"],
+} satisfies Meta<typeof DemoBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Red: Story = {
+  args: {
+    color: "red",
+    children: "Lorem Ipsum",
+  },
+};
+export const Blue: Story = {
+  args: {
+    color: "blue",
+    children: "Lorem Ipsum",
+  },
+};
+export const Yellow: Story = {
+  args: {
+    color: "yellow",
+    children: "Lorem Ipsum",
+  },
+};
