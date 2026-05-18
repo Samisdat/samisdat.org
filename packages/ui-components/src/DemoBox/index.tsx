4476e03d18cdc5527ee61fe198f163e93b7f7504
Samisdat <bastian@pertz.eu>
2026-05-18 19:55:06 +0200

    use css var

--- packages/ui-components/src/DemoBox/index.tsx
+++ packages/ui-components/src/DemoBox/index.tsx
@@ -1,39 +1,41 @@
-import { styled } from '@linaria/react';
-import { FC, HTMLAttributes } from 'react';
+import { styled } from "@linaria/react";
+import { FC, HTMLAttributes } from "react";
 
 const demoBoxColors = {
-    red: {
-        background: 'lab(56.32% 68.31 23.33)',
-    },
-    blue: {
-        background: 'lab(55.29% 14.38 -64.44)',
-    },
-    yellow: {
-        background: 'lab(70.04% 23.01 74.24)',
-    },
+  red: {
+    background: "var(--color-red)",
+  },
+  blue: {
+    background: "var(--color-blue)",
+  },
+  yellow: {
+    background: "var(--color-yellow)",
+  },
 };
 
 type DemoBoxColors = keyof typeof demoBoxColors;
 
 interface DemoBoxProps extends HTMLAttributes<HTMLDivElement> {
-    color?: DemoBoxColors;
+  color?: DemoBoxColors;
 }
 
 const DemoBoxStyling = styled.div<{
-    $color: DemoBoxColors;
+  $color: DemoBoxColors;
 }>`
-    padding: 1rem;
-    margin-bottom: 1rem;
-    background: ${props => demoBoxColors[props.$color].background};
+  padding: 1rem;
+  margin-bottom: 1rem;
+  background: ${(props) => demoBoxColors[props.$color].background};
+  color: contrast-color(${(props) => demoBoxColors[props.$color].background});
 `;
 
-export const DemoBox: FC<DemoBoxProps> = ({ color = 'blue', children, ...props }) => {
-    return (
-        <DemoBoxStyling
-            $color={color}
-            {...props}
-        >
-            {children}
-        </DemoBoxStyling>
-    );
+export const DemoBox: FC<DemoBoxProps> = ({
+  color = "blue",
+  children,
+  ...props
+}) => {
+  return (
+    <DemoBoxStyling $color={color} {...props}>
+      {children}
+    </DemoBoxStyling>
+  );
 };
