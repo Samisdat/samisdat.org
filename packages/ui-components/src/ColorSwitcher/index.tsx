c7e4ba640bc5002da2bf9419ff48d09e3dcf4675
Samisdat <bastian@pertz.eu>
2026-06-10 23:54:13 +0200

    ui-components importiert useMediaQuery aus tools

--- packages/ui-components/src/ColorSwitcher/index.tsx
+++ packages/ui-components/src/ColorSwitcher/index.tsx
@@ -1,54 +1,60 @@
-'use client';
-import { useMediaQuery } from '@/hooks/useMediaQuery';
-import { useEffect, useState } from 'react';
+"use client";
+import { useMediaQuery } from "@samisdat/tools";
+import { useEffect, useState } from "react";
 
-type ColorTheme = 'light' | 'dark';
+type ColorTheme = "light" | "dark";
 
 export const ColorSwitcher = () => {
-    const prefersLight = useMediaQuery(`(prefers-color-scheme: light)`, false);
+  const prefersLight = useMediaQuery(`(prefers-color-scheme: light)`, false);
 
-    const [theme, setTheme] = useState<ColorTheme>(() => {
-        if (typeof window === 'undefined') return 'dark';
-        return ((sessionStorage.getItem('theme') as ColorTheme) ?? prefersLight) ? 'light' : 'dark';
-    });
+  const [theme, setTheme] = useState<ColorTheme>(() => {
+    if (typeof window === "undefined") return "dark";
+    return ((sessionStorage.getItem("theme") as ColorTheme) ?? prefersLight)
+      ? "light"
+      : "dark";
+  });
 
-    useEffect(() => {
-        const root = document.documentElement;
+  useEffect(() => {
+    const root = document.documentElement;
 
-        if (prefersLight && 'light' === theme) {
-            root.removeAttribute('data-theme');
-        } else {
-            root.setAttribute('data-theme', theme);
-        }
+    if (prefersLight && "light" === theme) {
+      root.removeAttribute("data-theme");
+      return;
+    } else if (!prefersLight && "dark" === theme) {
+      root.removeAttribute("data-theme");
+      return;
+    } else {
+      root.setAttribute("data-theme", theme);
+    }
 
-        sessionStorage.setItem('theme', theme);
-    }, [theme]);
+    sessionStorage.setItem("theme", theme);
+  }, [theme]);
 
-    return (
-        <>
-            <label></label>
-            <label>
-                <input
-                    type={'radio'}
-                    value={'system'}
-                    checked={theme === 'light'}
-                    onChange={() => {
-                        setTheme('light');
-                    }}
-                />
-                Light
-            </label>
-            <label>
-                <input
-                    type={'radio'}
-                    value={'system'}
-                    checked={theme === 'dark'}
-                    onChange={() => {
-                        setTheme('dark');
-                    }}
-                />
-                Dark
-            </label>
-        </>
-    );
+  return (
+    <>
+      <label></label>
+      <label>
+        <input
+          type={"radio"}
+          value={"system"}
+          checked={theme === "light"}
+          onChange={() => {
+            setTheme("light");
+          }}
+        />
+        Light
+      </label>
+      <label>
+        <input
+          type={"radio"}
+          value={"system"}
+          checked={theme === "dark"}
+          onChange={() => {
+            setTheme("dark");
+          }}
+        />
+        Dark
+      </label>
+    </>
+  );
 };
