c2e39f1fddc49aac058567ad3d73e78e470de094
Samisdat <bastian@pertz.eu>
2026-06-21 12:03:51 +0200

    Container: add padding prop from space token

--- packages/ui-components/src/Container/index.tsx
+++ packages/ui-components/src/Container/index.tsx
@@ -1,11 +1,20 @@
-import { styled } from '@linaria/react';
-import { FC, HTMLAttributes } from 'react';
+import { styled } from "@linaria/react";
+import { HTMLAttributes } from "react";
+import { space, SpaceToken } from "../tokens/space";
 
-const ContainerStyling = styled.div`
-    width: min(100% - 2rem, 1024px);
-    margin-inline: auto;
+const ContainerStyling = styled.div<{
+  $padding?: SpaceToken;
+}>`
+  padding-top: ${(props) => props.$padding ?? space[0]};
+  padding-bottom: ${(props) => props.$padding ?? space[0]};
+  width: min(100% - 2rem, 1024px);
+  margin-inline: auto;
 `;
 
-export const Container: FC<HTMLAttributes<HTMLDivElement>> = ({ children }) => (
-    <ContainerStyling>{children}</ContainerStyling>
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+  padding?: SpaceToken;
+}
+
+export const Container = ({ padding, children }: ContainerProps) => (
+  <ContainerStyling $padding={padding}>{children}</ContainerStyling>
 );
