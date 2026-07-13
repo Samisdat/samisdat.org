c514b538661f4bbe5b5da6b9fce304693559951a
samisdat <bastian@pertz.eu>
2026-07-13 18:00:18 +0200

    ObfuscatedEmail: deprecated clip durch clip-path ersetzen

--- packages/website/src/components/ObfuscatedEmail.tsx
+++ packages/website/src/components/ObfuscatedEmail.tsx
@@ -34,7 +34,7 @@ const Decoy = styled.span`
     width: 1px;
     height: 1px;
     overflow: hidden;
-    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
     white-space: nowrap;
     pointer-events: none;
 `;
